56b5ea6a045004d8d441aebcbda81bc8602e3655
diogo-adao <diogoadao01x@gmail.com>
2024-10-30 20:37:08 +0000

    starting philosophers

---
+++ philo/philo.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diolivei <diolivei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/30 17:44:35 by diolivei          #+#    #+#             */
+/*   Updated: 2024/10/30 17:51:55 by diolivei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#endif
